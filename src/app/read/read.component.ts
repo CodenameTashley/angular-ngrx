135be5830306bafa551fa80a94d02aa2b75de764
Tashley Toocaram <tashleyvivesh@gmail.com>
2019-09-19 10:40:05 +0400

    Upload files

---
+++ src/app/read/read.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+
+import { Observable } from 'rxjs';
+import { Store } from '@ngrx/store';
+import { Tutorial } from './../models/tutorial.model';
+
+import * as TutorialActions from './../actions/tutorial.actions';
+
+import { AppState } from './../app.state';
+
+@Component({
+  selector: 'app-read',
+  templateUrl: './read.component.html',
+  styleUrls: ['./read.component.css']
+})
+export class ReadComponent implements OnInit {
+
+  tutorials: Observable<Tutorial[]>;
+
+  constructor(private store: Store<AppState>) {
+    this.tutorials = store.select('tutorial');
+  }
+
+  delTutorial(index) {
+    this.store.dispatch(new TutorialActions.RemoveTutorial(index));
+  }
+
+  ngOnInit() {
+  }
+
+}
